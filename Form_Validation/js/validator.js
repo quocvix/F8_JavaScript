7496e6ddf7e670ac97200c2d7c0982db6ccc5b10
quocvix <quocvixcode@gmail.com>
2023-06-30 14:48:26 +0700

    success

--- Form_Validation/js/validator.js
+++ Form_Validation/js/validator.js
@@ -29,6 +29,7 @@ function Validator(options) {
         for (var i=0; i < rules.length; ++i) {
             switch (inputElement.type) {
                 case 'radio':
+
                 case 'checkbox':
                     errorMessage = rules[i](
                         formElement.querySelector(rule.selector + ':checked')
@@ -85,8 +86,20 @@ function Validator(options) {
                         
                         switch(input.type) {
                             case 'radio':
-                            case 'checkox':
                                 values[input.name] = formElement.querySelector('input[name="' + input.name +'"]:checked').value;
+                                break;
+                            case 'checkbox':
+                                if (!input.matches(':checked')) {
+                                    values[input.name] = '';
+                                    return values;
+                                }
+                                if (!Array.isArray(values[input.name])) {
+                                    values[input.name] = [];
+                                }
+                                values[input.name].push(input.value);
+                                break;
+                            case 'file':
+                                values[input.name] = input.files;
                                 break;
                             default:
                                 values[input.name] = input.value;
